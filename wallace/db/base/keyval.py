a3fd78c4ffb033e391643f4f107dee6061d94577
Christopher Sira <cbsira@gmail.com>
2014-12-11 22:01:18 -0500

    keyval model - expose raw ident (in addition to existing db_key)

--- wallace/db/base/keyval.py
+++ wallace/db/base/keyval.py
@@ -10,7 +10,7 @@ class KeyValueModel(Model):
     @classmethod
     def fetch(cls, ident):
         inst = cls()
-        inst._set_db_key(ident)
+        inst._cbs_ident = ident
         inst.pull()
         return inst
 
@@ -22,37 +22,40 @@ class KeyValueModel(Model):
         inst = super(KeyValueModel, cls).construct(new=new, **kwargs)
 
         if not new:
-            inst._set_db_key(ident)
+            inst._cbs_ident = ident
 
         return inst
 
     def __init__(self):
         Model.__init__(self)
-        self._cbs_db_key = None
+        self._cbs_ident = None
+
+    @property
+    def ident(self):
+        if self._cbs_ident is None:
+            raise DoesNotExist('new model')
+        return self._cbs_ident
 
     @property
     def db_key(self):
-        if self._cbs_db_key is None:
-            raise DoesNotExist('new model')
-        return self._cbs_db_key
+        if self.prefix:
+            return '%s:%s' % (self.prefix, self.ident,)
+        return self.ident
 
 
     _create_new_ident = staticmethod(lambda: uuid.uuid4().hex)
     prefix = None
 
-    @classmethod
-    def _format_db_key(cls, ident):
-        return '%s:%s' % (cls.prefix, ident,) if cls.prefix else ident
 
-    def _set_db_key(self, ident):
-        self._cbs_db_key = self._format_db_key(ident)
-
+    def push(self, *a, **kw):
+        with self._new_model_key_handler():
+            super(KeyValueModel, self).push(*a, **kw)
 
     @contextmanager
     def _new_model_key_handler(self):
         reset_key_on_error = False
         if self.is_new:
-            self._set_db_key(self._create_new_ident())
+            self._cbs_ident = self._create_new_ident()
             reset_key_on_error = True
 
         try:
@@ -61,9 +64,5 @@ class KeyValueModel(Model):
             if reset_key_on_error:
                 # todo may make sense to do an existence check here (or catch
                 # a more specific error) so we don't end up with orphaned items
-                self._cbs_db_key = None
+                self._cbs_ident = None
             raise
-
-    def push(self, *a, **kw):
-        with self._new_model_key_handler():
-            super(KeyValueModel, self).push(*a, **kw)
