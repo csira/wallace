b14b2b562cf38ceaf27ffc867114f6e39f600d15
Christopher Sira <cbsira@gmail.com>
2015-04-22 10:00:08 -0400

    [db] override superclass default fields for non-DataType fields too

--- wallace/db/base/model.py
+++ wallace/db/base/model.py
@@ -6,6 +6,10 @@ from wallace.db.base.errors import DoesNotExist
 
 class Base(type):
     def __new__(cls, name, bases, dct):
+        for key, val in dct.iteritems():
+            if isinstance(val, DataType):
+                val.attr = key
+
         the_class = super(Base, cls).__new__(cls, name, bases, dct)
         the_class._cbs_default_fields = cls._get_defaults(bases, dct)
         return the_class
@@ -19,13 +23,10 @@ class Base(type):
                 defaults[key] = val
 
         for key, val in dct.iteritems():
-            if isinstance(val, DataType):
-                val.attr = key
-
-                if val.default is not None:
-                    defaults[key] = val.default
-                elif key in defaults:  # catch superclass default fields
-                    defaults.pop(key)  # overwridden here w/o a default
+            if isinstance(val, DataType) and val.default is not None:
+                defaults[key] = val.default
+            elif key in defaults:  # catch superclass fields declared with a
+                defaults.pop(key)  # default but overridden without one here
 
         return tuple(defaults.items())
 
