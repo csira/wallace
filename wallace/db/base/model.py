ea3a82353dd3cf65b8982c43f5283490239ef24a
Christopher Sira <cbsira@gmail.com>
2017-04-29 13:05:50 -0400

    split out default finder

--- wallace/db/base/model.py
+++ wallace/db/base/model.py
@@ -4,6 +4,29 @@ from wallace.db.base.attrs.base import DataType
 from wallace.errors import DoesNotExist
 
 
+def _get_default_fields(bases, dct):
+    """
+    Find model attributes with default values. The metaclass below adds them
+    to class scope so new instances initialize with the presets.
+
+    Merges in superclass defaults as well.
+
+    """
+    defaults = {}
+
+    for base in bases:  # for model inheritance
+        for key, val in getattr(base, "_cbs_default_fields", []):
+            defaults[key] = val
+
+    for key, val in dct.iteritems():
+        if isinstance(val, DataType) and val.default is not None:
+            defaults[key] = val.default
+        elif key in defaults:  # catch superclass fields originally declared with
+            defaults.pop(key)  # a default, but overridden here without one
+
+    return tuple(defaults.items())
+
+
 class Base(type):
 
     def __new__(cls, name, bases, dct):
@@ -12,24 +35,8 @@ class Base(type):
                 val.attr = key
 
         the_class = super(Base, cls).__new__(cls, name, bases, dct)
-        the_class._cbs_default_fields = cls._get_defaults(bases, dct)
+        the_class._cbs_default_fields = _get_default_fields(bases, dct)
         return the_class
-
-    @staticmethod
-    def _get_defaults(bases, dct):
-        defaults = {}
-
-        for base in bases:  # for model inheritance
-            for key, val in getattr(base, '_cbs_default_fields', []):
-                defaults[key] = val
-
-        for key, val in dct.iteritems():
-            if isinstance(val, DataType) and val.default is not None:
-                defaults[key] = val.default
-            elif key in defaults:  # catch superclass fields declared with a
-                defaults.pop(key)  # default but overridden without one
-
-        return tuple(defaults.items())
 
 
 class Model(object):
